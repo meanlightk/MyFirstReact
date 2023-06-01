6907f65c0b12edebb50f124df9014bf47d1d7b41
meanlightk <meanlightk@gmail.com>
2023-06-01 13:14:00 +0900

    state 개념

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -48,11 +48,18 @@ function Article(props) {
 }
 
 function App() {
+  const mode = "WELCOME";
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
   ]
+let content = null;
+if(mode === "WELCOME") {
+content = <Article title = "Welcome" body="Hello, Good Day"></Article>
+} else if (mode === "READ") {
+content = <Article title = "Welcome" body="Hello, Read"></Article>
+}
   return (
     <div>
       <Header title="WEB" onChangeMode={function() {
@@ -62,7 +69,7 @@ function App() {
       <Nav topics={topics} onChangeMode={(id) => {
         alert(id);
       }}></Nav>
-      <Article title="Welcome" body="Hello, Good Day!!!"></Article>
+      {content}
     </div>
   );
 }    
