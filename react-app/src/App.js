dcc6212f6b5d76bcf5e6f36baf9d235325d7e495
meanlightk <meanlightk@gmail.com>
2023-06-08 11:12:51 +0900

    Delete기능 추가

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -112,11 +112,29 @@ content = <Article title = "Welcome" body="Hello, Good Day"></Article>
       body = topics[i].body;
     } 
   } 
-content = <Article title = {title} body={body}></Article>
-contextControl = <li><a href={'/update/'+id} onClick={event=> {
-  event.preventDefault();
-  setMode("UPDATE");
-}} >Update</a></li>
+content = <Article title={title} body={body}></Article>
+contextControl = <>
+  <li>
+      <a href={'/update/'+id} onClick={event=> {
+        event.preventDefault();
+        setMode("UPDATE");
+        }}>Update
+      </a>
+  </li>
+
+  <li>
+      <input type="button" value="Delete" onClick={()=> {
+        const newTopics = []
+        for (let i = 0; i < topics.length; i++) {
+          if(topics[i].id !== id) {
+            newTopics.push(topics[i]);
+          }
+        }
+        setTopics(newTopics);
+      }} />
+  </li>
+</>
+
 } else if (mode === "CREATE") {
   content = <Create onCreate={(_title, _body)=> {
     const newTopic = {id:nextId, title:_title, body:_body}
