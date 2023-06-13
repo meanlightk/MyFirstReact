f21d3770d32d615bb4e339477b6e35dfa6745d6c
meanlightk <meanlightk@gmail.com>
2023-06-13 10:56:50 +0900

    Redux

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -1,93 +1,31 @@
-import React, { useState } from 'react';
-import './style.css';
-import {createStore} from 'redux';
-import {Provider, useSelector, useDispatch, connect} from 'react-redux';
-
-
-function reducer(currentState, action) {
-  if(currentState === undefined) {
-    return {
-      number: 1,
-    };
-  }
-  const newState = {...currentState};
-  if(action.type === 'PLUS') {
-    newState.number++;
-  }
-  return newState;
-}
-const store = createStore(reducer);
-
-
-export default function App() {
-  return (
-   <div id="container">
-      <h1>Root</h1>
-      <div id="grid">
-        <Provider store={store}>
-        <Left1></Left1>
-        <Right1></Right1>
-        </Provider>
-      </div>
-   </div>
-  );
+import {legacy_createStore as createStore} from  'redux';
+//import {createStore} from 'redux';
+import {Provider, useSelector, useDispatch} from 'react-redux';
+function reducer(state, action){
+    if(action.type === 'up'){
+      return{...state, value:state.value + action.step}
+    }
+    return state;
 }
 
-function Right1(props) {
-  return (
-    <div>
-      <h1>Right1</h1>
-      <Right2></Right2>
-    </div>
-  );
-}
+const initialState = {value:0}
+const store = createStore(reducer, initialState);
 
-function Right2(props) {
-  return (
-    <div>
-      <h1>Right2</h1>
-      <Right3></Right3>
-    </div>
-  );
-}
-
-function Right3(props) {
+function Counter(){
   const dispatch = useDispatch();
-  return (
-    <div>
-      <h1>Right3</h1>
-      <input type="button" value="+" onClick={() => {dispatch({type:'PLUS'});
-      }}></input>
-    </div>
-  );
+  const count = useSelector(state=>state.value);
+  return<div>
+    <button onClick={()=>{
+      dispatch({type:'up', step:2})
+    }}>+</button>{count}
+  </div>
 }
-
-
-function Left1(props) {
-  return (
-    <div>
-      <h1>Left1</h1>
-      <Left2></Left2>
-    </div>
-  );
-}
-
-function Left2(props) {
-  console.log('2');
-  return (
-    <div>
-      <h1>Left2</h1>
-      <Left3></Left3>
-    </div>
-  );
-}
-
-function Left3(props) {
-  console.log('3');
-  const number = useSelector((state) => state.number);
-  return (
-    <div>
-      <h1>Left3 : {number}</h1>
-    </div>
+export default function App() {
+  return(
+    <Provider store={store}>
+      <div>
+        <Counter></Counter>
+      </div>
+    </Provider>
   );
 }
