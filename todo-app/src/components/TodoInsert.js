c543d4ef3a431d632bc069788760a5cb85f5fcb7
meanlightk <meanlightk@gmail.com>
2023-06-22 15:30:36 +0900

    일정 항목 추가 기능 구현

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,10 +1,32 @@
+import { useState, useCallback } from 'react';
 import { CiEdit } from 'react-icons/ci';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue(''); // value값 초기화
+
+            // submit 이벤트는 브라우저에서 새로고침을 발생
+            // 이를 방지하기 위해 이 함수 호출
+            e.preventDefault();
+        },
+        [onInsert, value],
+    );
+
     return (
-        <form className="TodoInsert">
-            <input placeholder='할 일을 입력하세요' />
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input placeholder='할 일을 입력하세요'
+                value={value}
+                onChange={onChange}
+            />
             <button type='submit'>
                 <CiEdit />
             </button>
