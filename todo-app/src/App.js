4fbcc37802b10ec14687220884769d1723dd02cc
meanlightk <meanlightk@gmail.com>
2023-06-22 12:12:44 +0900

    일정 관리 항목 기능 구현

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -5,10 +6,28 @@ import './App.css';
 import './index.css'
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링 해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들기',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
