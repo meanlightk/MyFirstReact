58bf8ec318b60c1fcae627033a3f4ff050a6540c
meanlightk <meanlightk@gmail.com>
2023-06-27 12:00:24 +0900

    Hook - useNavigate

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,16 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Layout from './Layout';
 import About from './pages/About';
 import Home from './pages/Home';
-import './App.css';
 import Profile from './pages/Profile';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
-    </Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+    </Routes >
   );
 };
 
