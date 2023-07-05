be7beef1589c143cd14e735091a310fa72a52c0f
meanlightk <meanlightk@gmail.com>
2023-07-05 10:36:43 +0900

    미들웨어 적용하기 Redux-thunk 사용하기

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,17 +1,29 @@
-import { createAction, handleActions } from 'redux-actions';
+import { createAction, handleActions } from "redux-actions";
 
-const INCREASE = 'counter/INCREASE';
-const DECREASE = 'counter/DECREASE';
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
 
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요.
+// 1초 뒤에 increase 혹은 decrease 함수를 디스패치함
+export const increaseAsync = () => (dispatch) => {
+    setTimeout(() => {
+        dispatch(increase());
+    }, 1000);
+};
+export const decreaseAsync = () => (dispatch) => {
+    setTimeout(() => {
+        dispatch(decrease());
+    }, 1000);
+};
+
+const initialState = 0; // 상태는 꼭 객체일 필요가 없음, 숫자도 작동
 
 const counter = handleActions(
     {
-        [INCREASE]: state => state + 1,
-        [DECREASE]: state => state - 1
+        [INCREASE]: (state) => state + 1,
+        [DECREASE]: (state) => state - 1,
     },
     initialState
 );
